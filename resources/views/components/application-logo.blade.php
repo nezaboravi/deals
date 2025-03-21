9bc1a32721892136e32c9476a7659c6470cf7ae6
Vladimir Nikolic <vladimir@codingwisely.com>
2025-03-21 17:56:03 +0100

    WIP

---
+++ resources/views/components/application-logo.blade.php
@@ -0,0 +1,8 @@
+<svg viewBox="0 0 50 50" fill="none" xmlns="http://www.w3.org/2000/svg" {{ $attributes }}>
+    <path d="M25 5C13.9543 5 5 13.9543 5 25C5 36.0457 13.9543 45 25 45C36.0457 45 45 36.0457 45 25C45 13.9543 36.0457 5 25 5Z" fill="#FF2D20" fill-opacity="0.8"/>
+    <path d="M34.5 18L25.5 35L24.5 18H34.5Z" fill="white"/>
+    <path d="M15.5 18L24.5 35L25.5 18H15.5Z" fill="white"/>
+    <path d="M15.5 18H34.5L25 13L15.5 18Z" fill="white"/>
+    <path d="M25 35L15.5 18L25 30.5V35Z" fill="white" fill-opacity="0.5"/>
+    <path d="M25 35L34.5 18L25 30.5V35Z" fill="white" fill-opacity="0.5"/>
+</svg>
